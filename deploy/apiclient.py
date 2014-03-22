e8972f8d67132091f15c46371aaafdaeaea00d89
Marijn Giesen <marijn@studio-donder.nl>
2014-03-22 01:28:07 +0100

    Improvements

--- deploy/apiclient.py
+++ deploy/apiclient.py
@@ -4,33 +4,44 @@ import json
 
 
 class ApiClient:
-    def __init__(self):
-        pass
+	def __init__(self):
+		pass
 
 
-    def get_projects(self):
-        response = requests.get(registry.config["api"]["url"] + "/projects")
-        return response.json()
+	@staticmethod
+	def get_projects():
+		response = requests.get(registry.config["api"]["url"] + "/projects")
+		print response
+
+		return response.json()
 
 
-    def get_project(self, id):
-        response = requests.get(registry.config["api"]["url"] + "/projects/%s" % str(id))
-        return response.json()
+	@staticmethod
+	def get_project(id):
+		response = requests.get(registry.config["api"]["url"] + "/projects/%s" % str(id))
+		print response
+
+		return response.json()
 
 
-    def add_commit(self, project_id, commit_hash, date_added, message, author, changes):
-        payload = {"Hash": commit_hash, "DateAdded": date_added, "Message": message, "Author": author,
-                   "Changes": changes}
-        response = requests.post(registry.config["api"]["url"] + "/projects/%s/commit" % str(project_id),
-                                 data=json.dumps(payload))
-        return response
+	@staticmethod
+	def add_commit(project_id, commit_hash, date_added, message, author, changes):
+		payload = {"Hash": commit_hash, "DateAdded": date_added, "Message": message, "Author": author,
+		           "Changes": changes}
+		headers = {'content-type': 'application/json'}
+
+		response = requests.post(registry.config["api"]["url"] + "/projects/%s/commit/add" % str(project_id),
+		                         data=json.dumps(payload), headers=headers)
+		return response.json()
 
 
-    def get_queue(self):
-        response = requests.get(registry.config["api"]["url"] + "/queue")
-        return response.json()
+	@staticmethod
+	def get_queue():
+		response = requests.get(registry.config["api"]["url"] + "/queue")
+		return response.json()
 
 
-    def remove_from_queue(self, id):
-        response = requests.delete(registry.config["api"]["url"] + "/queue/%s" % str(id))
-        return response
+	@staticmethod
+	def remove_from_queue(id):
+		response = requests.delete(registry.config["api"]["url"] + "/queue/%s" % str(id))
+		return response
--- deploy/main.py
+++ deploy/main.py
@@ -1,38 +1,41 @@
 import sys
 import debug
 import daemon
+from apiclient import ApiClient
 import registry
-from repository import *
+# from repository import *
 
 
 def run():
-    repository = GitRepo("/tmp/repo", "https://github.com/marijngiesen/zabbix-ems.git")
-
-    remote_status = repository.check_for_new_commits_on_origin()
-    if remote_status == 1:
-        text = "commit"
-    else:
-        text = "commits"
-
-    debug.message("master is %d %s behind origin/master" % (remote_status, text))
-    if remote_status > 0:
-        repository.merge_origin()
+# repository = GitRepo("/tmp/repo", "https://github.com/marijngiesen/zabbix-ems.git")
+#
+# remote_status = repository.check_for_new_commits_on_origin()
+# if remote_status == 1:
+#     text = "commit"
+# else:
+#     text = "commits"
+#
+# debug.message("master is %d %s behind origin/master" % (remote_status, text))
+# if remote_status > 0:
+#     repository.merge_origin()
+	print ApiClient.get_projects()
+	print ApiClient.add_commit(1, "sdlksdfjklsdfd", "2013-03-21", "Test commit via api", "Marijn Giesen", "blablabla")
 
 
 def main():
-    foreground = False
+	foreground = False
 
-    for arg in sys.argv:
-        if "-d" in arg:
-            debug.enable(registry.process)
-        if "-l" in arg:
-            debug.open_log(registry.process, registry.logfile)
-        if "-f" in arg:
-            # daemon.run(run, foreground=True)
-            foreground = True
+	for arg in sys.argv:
+		if "-d" in arg:
+			debug.enable(registry.process)
+		if "-l" in arg:
+			debug.open_log(registry.process, registry.logfile)
+		if "-f" in arg:
+			# daemon.run(run, foreground=True)
+			foreground = True
 
-    daemon.start(run, foreground)
+	daemon.start(run, foreground)
 
 
 if __name__ == "__main__":
-    main()
+	main()
